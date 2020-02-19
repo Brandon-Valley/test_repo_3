e25f47b750b3262e860efa97381ca255b62ede7d
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-02-19 10:32:07 -0600

    uppdate

---
+++ post_merge_test.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+echo "start" >> out.txt
+
+
+TEMP_SUBMODULE_LIST_FILE_PATH="tempppppppppppppppppppppppp.txt"
+
+# echo $TEMP_SUBMODULE_LIST_FILE_PATH >> out.txt
+
+# cmd = "git config --file .gitmodules --get-regexp path | awk '{ print $2 }' > $TEMP_SUBMODULE_LIST_FILE_PATH"
+# eval $cmd
+
+# cmd="echo $TEMP_SUBMODULE_LIST_FILE_PATH >> out.txt"
+# eval $cmd
+
+
+
+
+
+# git config --file .gitmodules --get-regexp path | awk '{ print $2 }' > $TEMP_SUBMODULE_LIST_FILE_PATH
+
+
+
+# # write the path (relative to the directory that contains the .git file) to a temp file (that will be deleted later
+git config --file .gitmodules --get-regexp path | awk '{ print $2 }' > $TEMP_SUBMODULE_LIST_FILE_PATH
+
+
+# # read the contents of the temp file into an array
+readarray -t submodule_relative_paths_array < $TEMP_SUBMODULE_LIST_FILE_PATH
+
+
+
+
+for i in "${submodule_relative_paths_array[@]}"
+do
+   echo $i >> out.txt
+   git config -f .gitmodules submodule.$i.update rebase
+done
+
+
+
+
+# readarray -t submodule_a < cmd_out.txt
+
+# for i in "${submodule_a[@]}"
+# do
+	# echo $i >> out.txt
+# done
+
+
+
+
+
+
+
+
+echo "end" >> out.txt
